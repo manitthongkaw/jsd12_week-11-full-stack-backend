f4a0a83e8a7990e001579b2fa821492f110eadb9
manitthongkaw <manitthongkaw@gmail.com>
2026-05-13 14:53:49 +0700

    setup backend project

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: 1,
+    username: "Arm",
+    email: "arm@mail.org",
+    password: "pw123456",
+  },
+  {
+    id: 2,
+    username: "user2",
+    email: "user2@jsd12.org",
+    password: "jsd12",
+  }
+];
